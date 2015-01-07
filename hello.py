7a7e18006ebd393f8a99638910b1e013cc44289b
Jorge Escobar <jorge@fromzero.io>
2015-01-07 18:40:51 -0500

    added step-5

--- hello.py
+++ hello.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
+
+@app.route('/')
+def show_url_for():
+    # display the url for a function
+    return url_for('show_user_profile', username='jorge')
 
 @app.route('/user/<username>')
 def show_user_profile(username):
     # show the user profile for that user
     return 'User %s' % username
-
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
 
 if __name__ == '__main__':
     app.debug = True
