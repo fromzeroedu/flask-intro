25d34a9262732f605ad29028a41171c4a0707d9c
Jorge Escobar <jorge@fromzero.io>
2015-01-07 18:52:39 -0500

    added step-6

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
 from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET'])
 def login():
-    if request.method == 'POST':
+    if request.method == 'GET':
         return 'logged in'
     else:
-        return '<form method="post" action="/login"><input type="text" /><p><button type="submit">Submit</button></form>'
+        return '<form method="get" action="/login"><input type="text" /><p><button type="submit">Submit</button></form>'
 
 if __name__ == '__main__':
     app.debug = True
